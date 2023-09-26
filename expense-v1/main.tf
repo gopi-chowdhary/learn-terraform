26566bd25c5eaa1a121776d7c430f84743760ab2
gopi chowdhary <gopichowdhary99@gmail.com>
2023-09-26 19:52:28 +0530

    expense script

---
+++ expense-v1/main.tf
@@ -0,0 +1,53 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-093cdf2dfcca04ffb"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0315396BBDPQ9L5HOV7"
+  name    = "frontend.vdevops69.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.web.private_ip ]
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-093cdf2dfcca04ffb"]
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = "Z0315396BBDPQ9L5HOV7"
+  name    = "backend.vdevops69.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.backend.private_ip ]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-093cdf2dfcca04ffb"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0315396BBDPQ9L5HOV7"
+  name    = "mysql.vdevops69.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.mysql.private_ip ]
+}
